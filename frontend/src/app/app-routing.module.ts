cc9aba1221c5eaa6f35d3dec740f621a10528249
Dhafer-chihi <90657629+Dhafer-chihi@users.noreply.github.com>
2022-09-30 11:03:52 +0100

    Add files via upload

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
+import { LoginPageComponent } from './components/pages/login-page/login-page.component';
+import { ProductListComponent } from './components/pages/product-list/product-list.component';
+import { ProductPageComponent } from './components/pages/product-page/product-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '', component : ProductListComponent},
+  {path :'search/:searchTerm' , component : ProductListComponent },
+  {path : 'tag/:tag' , component : ProductListComponent},
+  {path : 'product/:id',component : ProductPageComponent},
+  {path : 'cart-page' , component : CartPageComponent},
+  {path : 'login' , component : LoginPageComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,16 +3,59 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/partials/header/header.component';
+import { FooterComponent } from './components/partials/footer/footer.component';
+import { ProductListComponent } from './components/pages/product-list/product-list.component';
+import { RatingModule } from 'ng-starrating';
+import { SearchComponent } from './components/partials/search/search.component';
+import { TagsComponent } from './components/partials/tags/tags.component';
+import { ProductPageComponent } from './components/pages/product-page/product-page.component';
+import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
+import { TitleComponent } from './components/partials/title/title.component';
+import { NotFoundComponent } from './components/partials/not-found/not-found.component';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginPageComponent } from './components/pages/login-page/login-page.component'
+import { ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { NgToastModule } from 'ng-angular-popup';
+import { InputContainerComponent } from './components/partials/input-container/input-container.component';
+import { InputValidationComponent } from './components/partials/input-validation/input-validation.component';
+import { TextInputComponent } from './components/partials/text-input/text-input.component';
+import { DefaultButtonComponent } from './components/partials/default-button/default-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductListComponent,
+    SearchComponent,
+    TagsComponent,
+    ProductPageComponent,
+    CartPageComponent,
+    TitleComponent,
+    NotFoundComponent,
+    LoginPageComponent,
+    InputContainerComponent,
+    InputValidationComponent,
+    TextInputComponent,
+    DefaultButtonComponent,
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    RatingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    ToastrModule,
+    NgToastModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+
 })
 export class AppModule { }
